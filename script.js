8f4007847c7737dffa91ceb8da8f0d1e7e06e974
danielepucci03 <danielepucci03@gmail.com>
2026-04-08 16:32:09 +0200

    add Event listener in js

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ console.log('Biglietto treno');
 const età = document.getElementById("age")
 
 const chilometri = document.getElementById("km")
+
+const bottone = document.getElementById("btn")
+
+bottone.addEventListener("click", function () {
+    
+    const age = parseInt(età.value)
+    const km = parseFloat(chilometri.value)
+
+    let prezzo = km * 0.21;
+
+    if (età < 18) {
+        prezzo = prezzo * 0.8
+    } else if (età > 65) {
+        prezzo = prezzo * 0.6
+    }
+    console.log("Prezzo finale" + prezzo.toFixed(2) + "€");
+    
+})
+
+
