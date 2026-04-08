5cc7b6120c501a6f6cb5b127ebc2f92bfa4f9602
danielepucci03 <danielepucci03@gmail.com>
2026-04-08 16:49:54 +0200

    add const risultato

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 console.log('Biglietto treno');
 
 const età = document.getElementById("age")
-
 const chilometri = document.getElementById("km")
-
 const bottone = document.getElementById("btn")
+const risultato = document.getElementById("risultato");
 
 bottone.addEventListener("click", function (e) {
     e.preventDefault()
     const age = parseInt(età.value)
     const km = parseFloat(chilometri.value)
+
+    if (isNaN(km) || isNaN(eta)) {
+    risultato.textContent = "Inserisci valori validi!";
+    return;
+  }
 
     let prezzo = km * 0.21;
 
